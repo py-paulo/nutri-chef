db1a413e32a0e1a9cc91db7eed934b170293df84
Paulo Roberto <paulo.rb.beserra@gmail.com>
2023-04-27 19:21:48 -0300

    feat: create class model to catalog and client webapp and web page static to clients;

--- app/catalog/models.py
+++ app/catalog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from client.models import Client
 
-# Create your models here.
+
+class TypeFood(models.Model):
+    name = models.CharField(max_length=50)
+  
+
+class Food(models.Model):
+    display_name = models.CharField(max_length=60)
+    name = models.CharField(max_length=30)
+    type_food = models.ForeignKey(TypeFood, on_delete=models.CASCADE)
+
+
+class FoodRecipe(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=4000)
+    foods = models.ManyToManyField(Food)
+
+
+class ClientFoodRecipe(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE)
+    food_recipe = models.ForeignKey(FoodRecipe, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now=True)
--- app/client/models.py
+++ app/client/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    full_name = models.CharField(max_lengh=255)
+    email = models.CharField(max_length=255)
+    phone = models.CharField(max_length=11)
+
