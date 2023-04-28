237099b67751a71bf96b56c29165059a85b4e577
Paulo Roberto <paulo.rb.beserra@gmail.com>
2023-04-28 19:22:11 -0300

    feat: página dinâmica de receita;

--- app/catalog/urls.py
+++ app/catalog/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.index, name="catalog"),
+    path("<int:recipe_id>/", views.recipe, name="recipe")
 ]
