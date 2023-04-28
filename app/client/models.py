0b48867a707ec5c634738eb2f3a672c821ebbf48
Paulo Roberto <paulo.rb.beserra@gmail.com>
2023-04-27 21:56:20 -0300

    fix: attribute in Client class;
    full_name = models.CharField(max_lengh=255)

--- app/client/models.py
+++ app/client/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Client(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
-    full_name = models.CharField(max_lengh=255)
+    full_name = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     phone = models.CharField(max_length=11)
 
