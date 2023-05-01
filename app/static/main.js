7d2bc0c9e0fef4f7a45966aa99a3ef15b288a962
Paulo Roberto <paulo.rb.beserra@gmail.com>
2023-05-01 08:04:25 -0300

    feat(mvp-1): search fields working;

--- app/static/main.js
+++ app/static/main.js
@@ -22,3 +22,31 @@ $('.delete')
     $('#delete-food-name').val(food_name);
     $('.ui.basic.modal').modal('show');
   });
+
+$("#client-search").on("input", function() {
+  let search_value = $(this).val();
+  let cards = $('.card');
+  for (let index = 0; index < cards.length; index += 1) {
+    let card = cards[index];
+    let client_name = card.getAttribute('data-client-name');
+    if (client_name.toLowerCase().startsWith(search_value.toLowerCase())) {
+      card.classList.remove("hidden");
+    } else {
+      card.classList.add("hidden");
+    }
+  }
+});
+
+$("#catalog-search").on("input", function() {
+  let search_value = $(this).val();
+  let cards = $('.card');
+  for (let index = 0; index < cards.length; index += 1) {
+    let card = cards[index];
+    let recipe_name = card.getAttribute('data-recipe-name');
+    if (recipe_name.toLowerCase().startsWith(search_value.toLowerCase())) {
+      card.classList.remove("hidden");
+    } else {
+      card.classList.add("hidden");
+    }
+  }
+});
